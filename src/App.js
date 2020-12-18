7656f8bb1e0e30da9fa76d7110e8d527becfd727
aboudaqrabawi <aqrabawijoe@gmail.com>
2020-12-18 20:21:34 +0200

    Creating the NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AdminProfile from './components/Profile/admin'
+import SellerProfile from './components/Profile/user'
+import AdminItems from './components/ListItems/admin'
+import SellerItems from './components/ListItems/user'
+import Homepage from './components/NavBar&homepage/homepage'
+import AddItems from './components/ListItems/addItems'
+import Sign from './components/Profile/Sign'
+
+
+
 
 function App() {
   return (
-    <div >
-      app
+    <Router className="container">
+    <div>
+      <Route path="/" component={Homepage} />
+      <Route path="/SellerItems" component={SellerItems} />
+      <Route path="/AdminItems" component={AdminItems} />
+      <Route path = "/AdminProfile"  component = {AdminProfile} />
+      <Route path="/SellerProfile" component={SellerProfile} />
+      <Route path="/AddItems" component={AddItems} />
+       <Route path="/sign"  component={Sign} />
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/NavBar&homepage/navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+    <div>
+    <nav>
+    <Link to="/" > DAWERHA </Link>
+    <Link to="/Items"> seller Items</Link>
+    <Link to="/sign" >sign in</Link>
+    <Link to="/AdminItems"> Admin Items </Link>
+    <Link to="/AddItems"> Add Items </Link>
+    <Link to="/AdminProfile"> Admin Profile </Link>
+    <Link to="/SellerProfile"> Seller Profile </Link>
+
+    </nav>
+    </div>
+    );
+  }
+}
