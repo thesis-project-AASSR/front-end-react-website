0b352df56b9680e3202d62ad0a9ecadc5e98b3fd
Saharalissa <sahar.alissa@gmail.com>
2020-12-20 20:35:33 +0200

    adding .env to gitignore file

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4000';
+const url = 'http://localhost:5000';
 
 export const fetchOrders = () => axios.get(url + '/ItemsList');
 export const createOrder = (newOrder) => axios.post(url+ '/items', newOrder);
