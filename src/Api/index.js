936332544153f4322e97fc965a5c81423ae55e7a
AreenBadran <areenbadran9@gmail.com>
2020-12-21 10:34:22 +0200

    front-end-authentication

--- src/Api/index.js
+++ src/Api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 //we use the same port of our heroku local server port
 const url = 'http://localhost:5000';
 
+//requests to routes
 export const fetchOrders = () => axios.get(url + '/ItemsList');
 export const createOrder = (newOrder) => axios.post(url+ '/items', newOrder);
 export const createUser = (newUser) => axios.post(url+ '/signup', newUser);
