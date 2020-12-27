d6bc288e595882331a29817745bbcbc2627d5946
aboudaqrabawi <aqrabawijoe@gmail.com>
2020-12-27 19:50:57 +0200

    front-end

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,5 +1,5 @@
 import  axios from 'axios';
-const url = 'http://localhost:5000';
+const url = 'https://backend-dawerha.herokuapp.com';
 //get all items
 export const fetchOrders = () => axios.get(url+'/ItemsList' );
 //create an item
