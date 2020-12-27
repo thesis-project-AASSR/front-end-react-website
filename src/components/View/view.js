9135b012090fa32124d2e94bba194e0c47c8292c
AreenBadran <areenbadran9@gmail.com>
2020-12-27 13:34:15 +0200

    styling with bootstrap-responsive

--- src/components/View/view.js
+++ src/components/View/view.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 // import Navbar from '../Navbar/navbar';
+import image1 from "../../images/nature-3294632_1280.jpg";
+import image2 from "../../images/96091161-wild-natural-forest-at-sunrise-the-sun-is-shining-through-the-trees.jpg";
+import image3 from "../../images/green-2008580_1280.jpg";
+import image4 from "../../images/black-and-white-1297353__480.webp";
+import image5 from "../../images/recycling.jpg";
+import image6 from "../../images/1_x9sm3fjasQp8gXQp-Sd0pA.png";
+import image7 from "../../images/forest-56930_1280.webp";
+import image8 from "../../images/lake-irene-1679708_1280.webp";
+
+
 export default class Viewpage extends Component {
   constructor(props) {
     super(props);
@@ -8,52 +18,57 @@ export default class Viewpage extends Component {
   }
   render() {
     return (
-    <div>
+  <div>
+
  <div>
       <div className="collapse bg-dark" id="navbarHeader">
         <div className="container">
           <div className="row">
             <div className="col-sm-8 col-md-7 py-4">
               <h4 className="text-white">About</h4>
-              <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+              <p className="text-muted">Dawerha&copy; is a nonprofit website with free Membership and everything posted must be free, legal and appropriate for all ages.
+               Dawerha is a recycling website which aims to encourage people to recycle more and recycle more often. Our  mission is to protect the environment by converting waste materials into new materials and objects.</p>
             </div>
             <div className="col-sm-4 offset-md-1 py-4">
               <h4 className="text-white">Contact</h4>
-              <ul className="list-unstyled">
-                <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                <li><a href="#" className="text-white">Like on Facebook</a></li>
-                <li><a href="#" className="text-white">Email me</a></li>
-              </ul>
+                <a href="https://m.facebook.com/" className="text-white" class="contact" style={{color:"rgba(0,255,0)"}}><i class="fab fa-facebook-square fa-1x"></i></a>
+               <a href="https://twitter.com/?lang=en" className="text-white" class="contact" style={{color:"rgba(0,255,0)"}}><i class="fab fa-twitter-square fa-1x"></i></a>
+               <a href="https://www.youtube.com/" className="text-white" class="contact" style={{color:"rgba(0,255,0)"}}><i class="fab fa-youtube fa-1x"></i></a>
+               <a href="https://mail.google.com/" className="text-white" class="contact" style={{color:"rgba(0,255,0)"}}><i class="fas fa-envelope-square fa-1x"></i></a>
+               <a href="https://www.instagram.com/" className="text-white" class="contact" style={{color:"rgba(0,255,0)"}}><i class="fab fa-instagram-square fa-1x"></i></a> 
+      
             </div>
           </div>
         </div>
       </div>
       <div className="navbar navbar-dark bg-dark box-shadow">
         <div className="container d-flex justify-content-between">
-          <a href="#" className="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>Album</strong>
+          <a href="/sign" className="navbar-brand d-flex align-items-center">
+            <img src={image6} width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2"></img>
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg> */}
+            <strong>Dawerha</strong>
           </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+
         </div>
       </div>
     </div>
-
-    {/* <main role="main">
-
+    
+  
       <section className="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+        <div className="container">
+          <h1 className="jumbotron-heading" style={{color:"black"}}>Dawerha</h1>
+          <p className="lead text-muted">can benefit our community and the environment, by collecting and processing materials that would otherwise be thrown away as trash and turning them into new products.</p>
           <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+            <a href="/sign" className="btn btn-outline-success">Join Us</a>
+            <a href="/sign" className="btn btn-outline-success" id="button">Sign in</a>
           </p>
         </div>
       </section>
 
+ {/* <main role="main">
       <div class="album py-5 bg-light">
         <div class="container">
 
@@ -201,18 +216,11 @@ export default class Viewpage extends Component {
 
     </main>
 
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
-      </div>
-    </footer> */}
-     <button onClick={() => {window.location='/sign'}}>sign up</button>
- <button onClick={() => {window.location='/sign'}}>sign in</button>
+*/}
+ 
+ 
     </div>
+    
     );
   }
 }
